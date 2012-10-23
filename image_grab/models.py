c127ba262698220524e6488c701c51df866a3a5c
Kris Fields <kris@avalaunch.com>
2012-10-22 23:56:01 -0400

    create vendor model

---
+++ image_grab/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Vendor(models.Model)
+	name = models.CharField(max_length=200)
+	domain_name = models.URLField(max_length=200)
+	profile_image_url = models.URLField(max_length=200)
+	facebook_id = models.PositiveIntegerField
+	facebook_url = models.URLField(max_length=200)
