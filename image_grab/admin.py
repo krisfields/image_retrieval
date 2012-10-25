658d74a88861d3ca773e91dbdcab4b77d0aa770b
Kris Fields <kris@avalaunch.com>
2012-10-25 16:18:08 -0400

    display profile image in admin

--- image_grab/admin.py
+++ image_grab/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from image_grab.models import *
 
 class VendorAdmin(admin.ModelAdmin):
-	list_display = ['domain_name', 'name', 'facebook_id', 'facebook_link', 'profile_image']
+	list_display = ['domain_name', 'profile_image', 'name', 'facebook_id', 'facebook_link', 'profile_image_url']
 	search_fields = ['domain_name', 'name']
 
 	def profile_image(self, obj):
 		if obj.profile_image_url:
-			return '<a href="%s">%s</a>' % (obj.profile_image_url, obj.profile_image_url)
+			return '<a href="%s"><img src="%s"></a>' % (obj.profile_image_url, obj.profile_image_url)
 		else:
 			return obj.profile_image_url
 	profile_image.allow_tags = True
-	profile_image.short_description = 'Profile image url'
+	profile_image.short_description = 'Profile image'
 
 	def facebook_link(self, obj):
 		if obj.facebook_url:
