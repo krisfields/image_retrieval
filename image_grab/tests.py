23fa620d0ea0d9c84952e637532528ed2bfc9077
Kris Fields <kris@avalaunch.com>
2012-10-26 18:37:49 -0400

    remove default test comments

--- image_grab/tests.py
+++ image_grab/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from image_grab.models import *
 from image_grab.views import _get_facebook_id as getFBID
